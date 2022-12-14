48b92deea278517ed73b0bfb118c9a483dfe4df6
stefaxel <62843391+stefaxel@users.noreply.github.com>
2022-12-14 17:03:42 +0000

    Changing how the input system works
    Changed from Send Messages to Invoke Unity Events

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -14,7 +14,7 @@ public class PlayerMovement : MonoBehaviour
     [Header("Player Conditions")]
     [SerializeField] float rayDistance;
     [SerializeField] LayerMask groundLayer;
-    Vector2 movement;
+    Vector2 movementX;
     Rigidbody2D rb;
 
     private void Awake()
@@ -24,26 +24,31 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        rb.AddForce(movement * movementSpeed * Time.deltaTime);
+        rb.AddForce(movementX * movementSpeed * Time.deltaTime);
     }
 
-    public void OnMove(InputValue value)
+    public void OnMove(InputAction.CallbackContext context)
     {
-        movement = value.Get<Vector2>();
+        
+        movementX = context.ReadValue<Vector2>();
+        
     }
 
-    public void OnJump(InputValue value)
+    public void OnJump(InputAction.CallbackContext context)
     {
-        if(IsGrounded() || canDoubleJump)
+        if(IsGrounded() && context.performed)
         {
+            Debug.Log("Spacebar pressed a first time");
             rb.AddForce(Vector2.up * jumpForce);
-            canDoubleJump = !canDoubleJump;
+            canDoubleJump = true;
         }
-        if(IsGrounded() && !value.isPressed)
+        if(context.action.triggered && !IsGrounded() && canDoubleJump)
         {
+            Debug.Log("Spacebar pressed a second time");
+            rb.AddForce(Vector2.up * jumpForce);
             canDoubleJump = false;
         }
-
+        
     }
 
     bool IsGrounded()
