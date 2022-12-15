77d4fe0fe4fd7fac525451fe4defce04fdbd7f94
stefaxel <62843391+stefaxel@users.noreply.github.com>
2022-12-15 19:34:52 +0000

    Added variable jump
    Changed gravity to affect the jump time and fall

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -10,11 +10,16 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float jumpForce = 300f;
     private bool canDoubleJump;
     private RaycastHit2D isPlayerOnGround;
-    
+    [SerializeField, Range(0f, 500f)] float decceleration;
+    [SerializeField, Range(0f, 500f)] float acceleration;
+    [SerializeField, Range(0f, 500f)] float targetSpeed;
+    [SerializeField, Range(0f, 5f)] float fallRate;
+    [SerializeField, Range(0f, 5f)] float lowJumpRate;
+
     [Header("Player Conditions")]
     [SerializeField] float rayDistance;
     [SerializeField] LayerMask groundLayer;
-    Vector2 movementX;
+    Vector2 movementInput;
     Rigidbody2D rb;
 
     private void Awake()
@@ -22,33 +27,53 @@ public class PlayerMovement : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
     }
 
+    private void Start()
+    {
+
+    }
+
     private void FixedUpdate()
     {
-        rb.AddForce(movementX * movementSpeed * Time.deltaTime);
+        //float speedDifference = targetSpeed - rb.velocity.x;
+        //float movement = speedDifference * acceleration;
+
+        rb.AddForce(movementInput * movementSpeed * Time.deltaTime);
+    }
+
+    private void Update()
+    {
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallRate - 1) * Time.deltaTime;
+        }
+        else if (rb.velocity.y > 0 && !Keyboard.current[Key.Space].IsActuated() 
+            || !Gamepad.current[UnityEngine.InputSystem.LowLevel.GamepadButton.South].IsActuated())
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpRate - 1) * Time.deltaTime;
+        }
     }
 
     public void OnMove(InputAction.CallbackContext context)
     {
-        
-        movementX = context.ReadValue<Vector2>();
-        
+        movementInput = context.ReadValue<Vector2>();
     }
 
     public void OnJump(InputAction.CallbackContext context)
     {
-        if(IsGrounded() && context.performed)
+
+        if (IsGrounded() && context.performed)
         {
-            Debug.Log("Spacebar pressed a first time");
+            //Debug.Log("Spacebar pressed a first time");
             rb.AddForce(Vector2.up * jumpForce);
             canDoubleJump = true;
         }
-        if(context.action.triggered && !IsGrounded() && canDoubleJump)
+        if (context.action.triggered && !IsGrounded() && canDoubleJump)
         {
-            Debug.Log("Spacebar pressed a second time");
+            //Debug.Log("Spacebar pressed a second time");
             rb.AddForce(Vector2.up * jumpForce);
             canDoubleJump = false;
         }
-        
+
     }
 
     bool IsGrounded()
