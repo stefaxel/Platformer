ae3bf868b971b27bec6b3382e7e6a65291e40383
stefaxel <62843391+stefaxel@users.noreply.github.com>
2022-12-19 19:44:48 +0000

    Gamepad is now compatible with the input system
    Trying to get an acceleration system to work

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,13 +6,12 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Player Movement")]
-    [SerializeField] float movementSpeed = 500f;
+    [SerializeField] float maxSpeed = 500f;
     [SerializeField] float jumpForce = 300f;
     private bool canDoubleJump;
     private RaycastHit2D isPlayerOnGround;
-    [SerializeField, Range(0f, 500f)] float decceleration;
-    [SerializeField, Range(0f, 500f)] float acceleration;
-    [SerializeField, Range(0f, 500f)] float targetSpeed;
+    [SerializeField, Range(0f, 10f)] float accelerationSpeed;
+    [SerializeField, Range(0f, 10f)] float decelerationSpeed;
     [SerializeField, Range(0f, 5f)] float fallRate;
     [SerializeField, Range(0f, 5f)] float lowJumpRate;
 
@@ -22,9 +21,20 @@ public class PlayerMovement : MonoBehaviour
     Vector2 movementInput;
     Rigidbody2D rb;
 
+    PlayerInput playerInput;
+    InputAction jumpAction;
+    InputAction moveAction;
+
+    bool changeDirection => (rb.velocity.x > 0f && moveAction.ReadValue<float>() < 0f)
+        || (rb.velocity.x < 0f && moveAction.ReadValue<float>() > 0f);
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        playerInput = GetComponent<PlayerInput>();
+        
+        jumpAction = playerInput.actions["Jump"];
+        moveAction = playerInput.actions["Move"];
     }
 
     private void Start()
@@ -34,10 +44,38 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
+        rb.AddForce(movementInput * maxSpeed * Time.deltaTime);
+
         //float speedDifference = targetSpeed - rb.velocity.x;
         //float movement = speedDifference * acceleration;
 
-        rb.AddForce(movementInput * movementSpeed * Time.deltaTime);
+        //rb.AddForce(movementInput * accelerationSpeed * Time.deltaTime);
+        //if (Mathf.Abs(rb.velocity.x) > maxSpeed)
+        //    rb.velocity = new Vector2(Mathf.Sign(rb.velocity.x) * maxSpeed, rb.velocity.y);
+
+        //if(Mathf.Abs(moveAction.ReadValue<float>()) < 0.4f || changeDirection)
+        //{
+        //    rb.drag = decelerationSpeed;
+        //}
+        //else
+        //{
+        //    rb.drag = 0f;
+        //}
+
+        //if (moveAction.IsPressed())
+        //{
+        //    playerVelocity += accelRate * Time.deltaTime;
+        //    playerVelocity = Mathf.Min(playerVelocity, maxSpeed);
+
+        //    rb.AddForce(movementInput * playerVelocity * Time.deltaTime);
+        //}
+        //if (!moveAction.IsPressed())
+        //{
+        //    playerVelocity += decelRate * Time.deltaTime;
+        //    playerVelocity = Mathf.Max(playerVelocity, 0);
+
+        //    rb.AddForce(movementInput * playerVelocity * Time.deltaTime);
+        //}
     }
 
     private void Update()
@@ -46,11 +84,11 @@ public class PlayerMovement : MonoBehaviour
         {
             rb.velocity += Vector2.up * Physics2D.gravity.y * (fallRate - 1) * Time.deltaTime;
         }
-        else if (rb.velocity.y > 0 && !Keyboard.current[Key.Space].IsActuated())
+        else if (rb.velocity.y > 0 && !jumpAction.IsPressed())
         {
             rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpRate - 1) * Time.deltaTime;
         }
-        //Keyboard.current[Key.Space].IsActuated() || !Gamepad.current[UnityEngine.InputSystem.LowLevel.GamepadButton.South].IsActuated()
+
     }
 
     public void OnMove(InputAction.CallbackContext context)
