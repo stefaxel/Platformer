fb1ab66906cc9e628a3e6f5b042bd1e437731736
stefaxel <62843391+stefaxel@users.noreply.github.com>
2023-01-23 23:44:13 +0000

    Adding more animations

--- Assets/Scripts/Traps/PlatformBounce.cs
+++ Assets/Scripts/Traps/PlatformBounce.cs
@@ -5,13 +5,28 @@ using UnityEngine;
 public class PlatformBounce : MonoBehaviour
 {
     [SerializeField] float upwardsForce;
-    
+    private Animator bounceAnimator;
 
+    bool bounceAnimation;
+
+    private void Start()
+    {
+        bounceAnimator = GetComponent<Animator>();
+    }
+
+    
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.name == "Player")
         {
+            bounceAnimator.SetBool("bounce player", true);
             collision.rigidbody.AddForce(Vector2.up * upwardsForce, ForceMode2D.Impulse);
+            SetIdleAnimation();
         }
+    }
+
+    private void SetIdleAnimation()
+    {
+        bounceAnimator.SetBool("bounce player", false);
     }
 }
